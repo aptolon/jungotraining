a2696ecccf5a1750259a6ae4c5e62cf6e2d63371
aptolon <naumovd127@gmail.com>
2024-09-02 15:35:54 +0300

    Описание изменений

--- store/products/models.py
+++ store/products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quanity = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='products_images', blank=True, null=True)
+    category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Продукт: {self.name} | Категория: {self.category.name}'
+    
--- store/store/settings.py
+++ store/store/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'products',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -123,7 +124,14 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# User
+AUTH_USER_MODEL = 'users.User'
